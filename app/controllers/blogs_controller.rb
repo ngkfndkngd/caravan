4a47c1549773115374de7b0f71c8b05dc77bf8ea
ngkfndkngd <huku.0033@gmail.com>
2021-09-11 08:47:45 +0000

    [Start] 2-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,7 +20,7 @@ class BlogsController < ApplicationController
     blog.save
     redirect_to blogs_path
   end
-  
+
   def edit
      @blog = Blog.find(params[:id])
   end
@@ -28,14 +28,14 @@ class BlogsController < ApplicationController
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
-    redirect_to blog_path(blog.id)
-    
+    redirect_to blog_path(blog)
+
   end
-  
+
     private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
 
-  
+
 end
