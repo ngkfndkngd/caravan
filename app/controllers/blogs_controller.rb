28f972d0458b99bf3d2a12550820f35502cca545
ngkfndkngd <huku.0033@gmail.com>
2021-09-11 10:03:44 +0000

    [Finish] 2-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,9 +16,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-   blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path
+   @blog = Blog.new(blog_params)
+   if @blog.save
+    redirect_to blogs_path(@blog.id)
+   else
+    render :new
+   end
   end
 
   def edit
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
-    
+validates :title, presence: true
+  validates :category, presence: true
+  validates :body, presence: true
 end
